3a10b3c0a645dcd6d1c0d588379972b5fbaa36c4
PilGyu-Jung <panther9596@hanmail.net>
2021-11-24 09:34:44 +0900

    monster1,2 추가
    추후에 boss와 이펙트, 사운드 추가예정.

---
+++ Assets/Script/Enemy/SearchArea.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SearchArea : MonoBehaviour {
+	MonsterCtrl enemyCtrl;
+	void Start()
+	{
+		// EnemyCtrl을 미리 준비한다.
+		enemyCtrl = transform.root.GetComponent<MonsterCtrl>();
+	}
+	
+	void OnTriggerStay( Collider other )
+	{
+        // Player태그를 타깃으로 한다.
+		if( other.tag == "Player" )
+			enemyCtrl.SetAttackTarget( other.transform );
+	}
+}
