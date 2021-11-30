c96e273ec160bea8400923b8e8a3c043f7fc2cab
LyuJaeHyuck <ljh990215@naver.com>
2021-11-30 23:14:02 +0900

    Attack Effect
    몬스터에게 공격적중시
    이펙트 나오도록 설정
    다른 이펙트는 이후 추가할 예정

--- Assets/Script/Character/CHAR_AttackArea.cs
+++ Assets/Script/Character/CHAR_AttackArea.cs
@@ -6,7 +6,8 @@ public class CHAR_AttackArea : MonoBehaviour
 {
     CHAR_CharacterStatus status;
 
-	public GameObject rooot;
+    public GameObject hitEffect;
+    public GameObject rooot;
     void Start()
     {
         status = transform.root.GetComponent<CHAR_CharacterStatus>();
@@ -41,7 +42,10 @@ public class CHAR_AttackArea : MonoBehaviour
 		// 공격 당한 상대의 Damage 메시지를 보낸다.
 		Debug.Log("들어갔다");
 		other.SendMessage("Damage",GetAttackInfo());
-		Debug.Log("보내졌다");
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity) as GameObject;
+        effect.transform.localPosition = transform.position + new Vector3(0.5f, 0.5f, 0.0f);
+        Destroy(effect, 0.3f); //이펙트
+        Debug.Log("보내졌다");
 		status.lastAttackTarget = other.transform.root.gameObject;
 	}
 
