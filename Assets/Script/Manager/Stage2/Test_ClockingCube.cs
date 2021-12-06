517820cbd5f574341465975d7b2c73cfe317c357
LyuJaeHyuck <ljh990215@naver.com>
2021-12-06 20:03:41 +0900

    2스테이지 사망
    함정 충돌시 페이드인아웃

--- Assets/Script/Manager/Stage2/Test_ClockingCube.cs
+++ Assets/Script/Manager/Stage2/Test_ClockingCube.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Test_ClockingCube : MonoBehaviour
 {
@@ -9,12 +10,18 @@ public class Test_ClockingCube : MonoBehaviour
     public bool hasGlass;
 
     public GameObject Player;
+    GameObject DeadPanel;
+    Image Panel;
+    float time = 0f;
+    float F_time = 1f;
     // Start is called before the first frame update
     void Start()
     {
         isClocking = false;
         hasGlass = false;
         Player = GameObject.FindWithTag("Player");
+        DeadPanel = GameObject.FindWithTag("deadPanel");
+        Panel = DeadPanel.GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -47,9 +54,27 @@ public class Test_ClockingCube : MonoBehaviour
 
     IEnumerator ResetPosition()
     {
+        time = 0f;
+        Color alpha = Panel.color;
+        while (alpha.a < 1f)
+        {
+            time += Time.deltaTime / F_time;
+            alpha.a = Mathf.Lerp(0, 1, time);
+            Panel.color = alpha;
+            yield return null;
+        }
+        time = 0f;
 
-        yield return new WaitForSeconds(2.0f);
-
+        yield return new WaitForSeconds(0.5f);
         Player.transform.position = new Vector3(-35.0f, 0.0f, 16.5f);
+
+        while (alpha.a > 0f)
+        {
+            time += Time.deltaTime / F_time;
+            alpha.a = Mathf.Lerp(1, 0, time);
+            Panel.color = alpha;
+            yield return null;
+        }
     }
+
 }
