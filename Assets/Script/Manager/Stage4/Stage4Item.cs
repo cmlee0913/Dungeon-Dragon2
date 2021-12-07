59b638b6ef5cccbcfa91e664049f95a09fee517d
KDB1020 <wlsrpr@gmail.com>
2021-12-08 00:14:29 +0900

    stage4 몬스터 데미지 받지 않는 오류 수정
    * 아직 움직임이 이상한 오류가 남음
    * 죽이자마자 아이템을 먹어버려서 아이템 확인이 안되는 문제가 있음

--- Assets/Script/Manager/Stage4/Stage4Item.cs
+++ Assets/Script/Manager/Stage4/Stage4Item.cs
@@ -9,7 +9,8 @@ public class Stage4Item : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.gameObject.transform.rotation = Quaternion.Euler(-90f, 0, 0);
+        this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, 1.0f, this.gameObject.transform.position.z);
     }
 
     // Update is called once per frame
