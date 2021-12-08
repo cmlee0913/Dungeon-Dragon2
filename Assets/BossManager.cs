a128845dc781d03d401234f8e486d1d8b63f8d2d
leecmin <cmlee0913@naver.com>
2021-12-08 19:58:59 +0900

    FEAT: 보스 씬 애니메이션 완성

--- Assets/BossManager.cs
+++ Assets/BossManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 enum Phase {
-    Phase0, Phase1, Phase2, Phase3
+    Phase0, Phase1, Phase2
 };
 
 public class BossManager : MonoBehaviour
@@ -11,8 +11,13 @@ public class BossManager : MonoBehaviour
     public static BossManager instance;
     public CharacterStatus characterStatus;
     public BossAnimation bossAnimation;
+    public MoveToPlayer moveToPlayer;
+    public HitArea hitArea;
+    public AttackRange attackRange;
     Phase phase = Phase.Phase0;
     public bool isUnbeatable;
+
+    bool phase1Start, phase2Start, phase1End, phase2End;
 
     void Awake() {
         if (instance == null) {
@@ -22,6 +27,9 @@ public class BossManager : MonoBehaviour
             Destroy(this.gameObject);
         }
 
+        hitArea = GetComponentInChildren<HitArea>();
+        attackRange = GetComponentInChildren<AttackRange>();
+        moveToPlayer = GetComponent<MoveToPlayer>();
         characterStatus = GetComponent<CharacterStatus>();
         bossAnimation = GetComponent<BossAnimation>();
     }
@@ -31,35 +39,76 @@ public class BossManager : MonoBehaviour
     }
 
     void Update() {
-        
+        PhaseCheck();
+        if (Input.GetKeyDown(KeyCode.R)) {
+            phase1End = true;
+            phase2End = true;
+        }
     }
 
     void PhaseCheck() {
-        
+        if (characterStatus.HP <= (characterStatus.MaxHP * 2) / 3 && !phase1Start && !isUnbeatable) {
+            UnbeatableMode();
+            phase = Phase.Phase1;
+            Phase1Gimmik();
+        }
+        if (characterStatus.HP <= characterStatus.MaxHP / 3 && !phase2Start && !isUnbeatable) {
+            UnbeatableMode();
+            phase = Phase.Phase2;
+            Phase2Gimmik();
+        }
+
+        if ((phase1End || phase2End) && isUnbeatable) {
+            phase1End = false;
+            phase2End = false;
+
+            isUnbeatable = false;
+
+            Invoke("OnInspector", 5f);
+
+            bossAnimation.animator.SetBool("isFlying", isUnbeatable);
+            bossAnimation.animator.SetTrigger("FlyingEnd");
+
+            phase = Phase.Phase0;
+            Phase0Gimmik();
+        }
+    }
+
+    void Phase0Gimmik() {
+        if (phase == Phase.Phase0) {
+            // 페이즈 1, 2 기믹 삭제하기
+            // if (기믹1 or 기믹2)
+            //    Destroy(기믹1 or 기믹2);
+        }
     }
 
     void Phase1Gimmik() {
         if (phase == Phase.Phase1) {
-
+            phase1Start = true;
         }
+
+        // if (기믹 조건이 완료된다면) {
+        //    phase1End = true;
+        //}
     }
 
     void Phase2Gimmik() {
         if (phase == Phase.Phase2) {
-            
+            phase2Start = true;
         }
-    }
 
-    void Phase3Gimmik() {
-        if (phase == Phase.Phase3) {
-            
-        }
+        // if (기믹 조건이 완료된다면) {
+        //    phase2End = true;
+        //}
     }
 
     void UnbeatableMode() {
-        if (isUnbeatable) {
+        isUnbeatable = true;
 
-        }
+        OffInspector();
+
+        bossAnimation.animator.SetBool("isFlying", isUnbeatable);
+        bossAnimation.animator.SetTrigger("FlyingStart");
     }
 
     public void Damage(CHAR_AttackArea.AttackInfo attackInfo) {
@@ -73,6 +122,24 @@ public class BossManager : MonoBehaviour
 
     void Died() {
         characterStatus.died = true;
+
+        OffInspector();
+
+        bossAnimation.animator.SetTrigger("Died");
 		StageControl.Instance.bossClear = true;
 	}
+
+    void OnInspector() {
+        attackRange.enabled = true;
+        hitArea.enabled = true;
+        GetComponent<BoxCollider>().enabled = true;
+        moveToPlayer.enabled = true;
+    }
+
+    void OffInspector() {
+        attackRange.enabled = false;
+        hitArea.enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
+        moveToPlayer.enabled = false;
+    }
 }
--- Assets/Script/Enemy/MonsterMove.cs
+++ Assets/Script/Enemy/MonsterMove.cs
@@ -5,82 +5,131 @@ using System.Collections;
 
 public class MonsterMove : MonoBehaviour
 {
+	// �߷°�.
 	const float GravityPower = 9.8f;
+	//���������� �����ߴٰ� ���� ���� �Ÿ�.
 	const float StoppingDistance = 0.006f;
 
+	// ���� �̵� �ӵ�.
 	Vector3 velocity = Vector3.zero;
+	// ĳ���� ��Ʈ�ѷ��� ĳ��.
+	CharacterController characterController;
+	// �����ߴ°�(�����ߴ� true / �������� �ʾҴ� false).
 	public bool arrived = false;
 
+	// ������ ������ �����ϴ°�.
 	bool forceRotate = false;
 
+	// ������ ���ϰ� �ϰ� ���� ����.
 	Vector3 forceRotateDirection;
 
+	// ������.
 	public Vector3 destination;
 
+	// �̵� �ӵ�.
 	public float walkSpeed = 6.0f;
 
+	// ȸ�� �ӵ�.
 	public float rotationSpeed = 360.0f;
 
+
+
+	// Use this for initialization
 	void Start()
 	{
+		characterController = GetComponent<CharacterController>();
 		destination = transform.position;
 	}
 
+	// Update is called once per frame
 	void Update()
 	{
-		Vector3 destinationXZ = destination;
-		destinationXZ.y = transform.position.y;
 
-		Vector3 direction = (destinationXZ - transform.position).normalized;
-		float distance = Vector3.Distance(transform.position, destinationXZ);
-
-		Vector3 currentVelocity = velocity;
-
-		if (distance < StoppingDistance)
+		// �̵� �ӵ� velocity�� �����Ѵ�.
+		if (characterController.isGrounded)
 		{
-			arrived = true;
-		}
+			// ����鿡�� �̵��� ����ϹǷ� XZ�� �ٷ��.
+			Vector3 destinationXZ = destination;
+			// �������� ���� ��ġ ���̸� �Ȱ��� �Ѵ�.
+			destinationXZ.y = transform.position.y;
 
-		if (arrived)
-			velocity = Vector3.zero;
-		else
-			velocity = direction * walkSpeed;
+			//********* ���⼭���� XZ������ �����Ѵ�. ********
+			// ���������� �Ÿ��� ������ ���Ѵ�.
+			Vector3 direction = (destinationXZ - transform.position).normalized;
+			float distance = Vector3.Distance(transform.position, destinationXZ);
 
-		velocity = Vector3.Lerp(currentVelocity, velocity, Mathf.Min(Time.deltaTime * 5.0f, 1.0f));
-		velocity.y = 0;
+			// ���� �ӵ��� �����Ѵ�.
+			Vector3 currentVelocity = velocity;
 
-
-		if (!forceRotate)
-		{
-			if (velocity.magnitude > 0.1f && !arrived)
+			//���������� ������ ������ ����.
+			if (distance < StoppingDistance)
 			{
-				Quaternion characterTargetRotation = Quaternion.LookRotation(direction);
+				arrived = true;
+				//Debug.Log($"��ġ �Ÿ���:{distance }");
+			}
+
+
+			// �̵� �ӵ��� ���Ѵ�.
+			if (arrived)
+				velocity = Vector3.zero;
+			else
+				velocity = direction * walkSpeed;
+
+
+			// �ε巴�� ���� ó��.
+			velocity = Vector3.Lerp(currentVelocity, velocity, Mathf.Min(Time.deltaTime * 5.0f, 1.0f));
+			velocity.y = 0;
+
+
+			if (!forceRotate)
+			{
+				// �ٲٰ� ���� �������� �����Ѵ�. 
+				if (velocity.magnitude > 0.1f && !arrived)
+				{
+					// �̵����� �ʾҴٸ� ������ �������� �ʴ´�.
+					Quaternion characterTargetRotation = Quaternion.LookRotation(direction);
+					transform.rotation = Quaternion.RotateTowards(transform.rotation, characterTargetRotation, rotationSpeed * Time.deltaTime);
+				}
+			}
+			else
+			{
+				// ������ ������ �����Ѵ�.
+				Quaternion characterTargetRotation = Quaternion.LookRotation(forceRotateDirection);
 				transform.rotation = Quaternion.RotateTowards(transform.rotation, characterTargetRotation, rotationSpeed * Time.deltaTime);
 			}
-		}
-		else
-		{
-			Quaternion characterTargetRotation = Quaternion.LookRotation(forceRotateDirection);
-			transform.rotation = Quaternion.RotateTowards(transform.rotation, characterTargetRotation, rotationSpeed * Time.deltaTime);
+
 		}
 
+		// �߷�.
 		velocity += Vector3.down * GravityPower * Time.deltaTime;
 
+		// ���� ��� �ִٸ� ������ �� ������.
+		// (����Ƽ�� CharactorController Ư�� ������).
 		Vector3 snapGround = Vector3.zero;
+		if (characterController.isGrounded)
+			snapGround = Vector3.down;
 
-		//if (characterController.velocity.magnitude < 0.1f)
+		// CharacterController�� ����ؼ� �����δ�.
+		characterController.Move(velocity * Time.deltaTime + snapGround);
+
+		if (characterController.velocity.magnitude < 0.1f)
 			arrived = true;
 
+		// ������ ���� ������ �����Ѵ�.
 		if (forceRotate && Vector3.Dot(transform.forward, forceRotateDirection) > 0.99f)
 			forceRotate = false;
+
+
 	}
 
+	// �������� �����Ѵ�. �μ� destination�� ������.
 	public void SetDestination(Vector3 destination)
 	{
 		arrived = false;
 		this.destination = destination;
 	}
 
+	// ������ �������� ���Ѵ�.
 	public void SetDirection(Vector3 direction)
 	{
 		forceRotateDirection = direction;
@@ -89,13 +138,18 @@ public class MonsterMove : MonoBehaviour
 		forceRotate = true;
 	}
 
+	// �̵��� �׸��д�.
 	public void StopMove()
 	{
+		// ���� ������ �������� �Ѵ�.
 		destination = transform.position;
 	}
 
+	// �������� �����ߴ��� �����Ѵ�(�����ߴ� true / �������� �ʾҴ� false).
 	public bool Arrived()
 	{
 		return arrived;
 	}
+
+
 }
